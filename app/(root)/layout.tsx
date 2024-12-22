015ad25cc7c796926bc5b1a36ddfa31fe50ba177
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2024-12-22 16:47:48 +0530

    complete application

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GangaLinkUp",
+  description:
+    "GangaLinkUp is a dynamic web application built with Next.js, designed to foster seamless, real-time connections and conversations. Inspired by the concept of interconnectedness, just like the flow of the Ganges River, GangaLinkUp enables users to engage, share, and communicate effortlessly in an intuitive, fast-paced environment. Whether you're posting updates, joining discussions, or connecting with friends, GangaLinkUp is the place to create meaningful interactions and stay connected.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
